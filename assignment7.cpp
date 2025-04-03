7f483c5fddd6851c1f5ef4a52430d48e84f6c01b
rishipatwa17p <rishi17p@gmail.com>
2025-04-03 18:08:39 +0530

    Update assignment7.cpp

--- assignment7.cpp
+++ assignment7.cpp
@@ -1,42 +1,50 @@
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
-int spanningTree(int V, vector<vector<int>> adj[]) {
-    vector<bool>vis(V,false);
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-    pq.push({0,0});
-    int sum=0;
-    while(!pq.empty()){
-        auto it=pq.top();
+int spanningTree(int V, vector<vector<int>> adj[])
+{
+    vector<bool> vis(V, false);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, 0});
+    int sum = 0;
+    while (!pq.empty())
+    {
+        auto it = pq.top();
         pq.pop();
-        int node=it.second;
-        int wt=it.first;
-        if(vis[node]) continue;
-        vis[node]=true;
-        sum+=wt;
-        for(auto it : adj[node]){
-            int neighbour=it[0];
-            int edge=it[1];
-            if(!vis[neighbour]){
-                pq.push({edge,neighbour});
+        int node = it.second;
+        int wt = it.first;
+        if (vis[node])
+            continue;
+        vis[node] = true;
+        sum += wt;
+        for (auto it : adj[node])
+        {
+            int neighbour = it[0];
+            int edge = it[1];
+            if (!vis[neighbour])
+            {
+                pq.push({edge, neighbour});
             }
         }
     }
     return sum;
 }
-int main() {
+int main()
+{
     int V, E;
     cin >> V >> E;
-    
+
     vector<vector<int>> adj[V];
 
-    for (int i = 0; i < E; i++) {
+    for (int i = 0; i < E; i++)
+    {
         int u, v, w;
         cin >> u >> v >> w;
+        u--;
+        v--;
         adj[u].push_back({v, w});
         adj[v].push_back({u, w});
     }
 
-    cout << "Minimum Spanning Tree Weight: " << spanningTree(V, adj) << endl;
-    return 0;
+    cout<<spanningTree(V, adj)<<endl;
 }
