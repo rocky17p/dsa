c86cc047a8e8efbca3608a866ce4101e48b17770
rishipatwa17p <rishi17p@gmail.com>
2025-04-19 16:15:14 +0530

    Update assignment07.cpp

--- assignment07.cpp
+++ assignment07.cpp
@@ -1,165 +1,176 @@
 #include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-class Edge {
+class edge
+{
 public:
-    int u, v, weight;
-    Edge(int u, int v, int w) {
+    int u;
+    int v;
+    int w;
+    edge(int u, int v, int w)
+    {
         this->u = u;
         this->v = v;
-        this->weight = w;
+        this->w = w;
     }
 };
-
-bool compare(Edge a, Edge b) {
-    return a.weight < b.weight;
+bool compare(edge a, edge b)
+{
+    return a.w < b.w;
 }
-
-class Graph {
-    int V;
-    vector<vector<pair<int, int>>> adj;
-    vector<Edge> edges;
-
+class graph
+{
 public:
-    Graph(int vertices) {
-        V = vertices;
-        adj.resize(V + 1); // 1-based indexing
+    int v;
+    vector<vector<pair<int, int>>> adj;
+    vector<edge> edges;
+    graph(int v)
+    {
+        this->v = v;
+        adj.resize(v + 1);
     }
-
-    void addEdge(int u, int v, int w) {
+    void addedge(int u, int v, int w)
+    {
         adj[u].push_back({v, w});
         adj[v].push_back({u, w});
-        edges.push_back(Edge(u, v, w));
+        edges.push_back(edge(u, v, w));
     }
-
-    void displayGraph() {
-        cout << "\nGraph Adjacency List:\n";
-        for (int i = 1; i <= V; i++) {
-            cout << "Node " << i << " -> ";
-            for (auto it : adj[i]) {
-                cout << "(" << it.first << ", " << it.second << ") ";
+    void prims()
+    {
+        int mst = 0;
+        vector<bool> visited(v + 1);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, 1});
+        while (!pq.empty())
+        {
+            auto it = pq.top();
+            pq.pop();
+            int wt = it.first;
+            int node = it.second;
+            if (visited[node])
+                continue;
+            visited[node] = true;
+            mst += wt;
+            for (auto it : adj[node])
+            {
+                int edgew = it.second;
+                int neigh = it.first;
+                if (!visited[neigh])
+                {
+                    pq.push({edgew, neigh});
+                }
+            }
+        }
+        cout << "mst cost is  " << mst << endl;
+    }
+    int findparent(int node, vector<int> &parent)
+    {
+        while (node != parent[node])
+        {
+            node = parent[node];
+        }
+        return parent[node];
+    }
+    void kruskals()
+    {
+        int mst = 0;
+        vector<int> parent(v + 1);
+        for (int i = 0; i <= v; i++)
+        {
+            parent[i] = i;
+        }
+        sort(edges.begin(), edges.end(), compare);
+        cout << "edges in mst" << endl;
+        for (auto s : edges)
+        {
+            int u = s.u;
+            int v = s.v;
+            int w = s.w;
+            int pu = findparent(s.u, parent);
+            int pv = findparent(s.v, parent);
+            if (pu != pv)
+            {
+                cout << u << "-" << v << ":" << w << endl;
+                mst += w;
+                parent[pu] = pv;
+            }
+        }
+        cout << "mst cost is  " << mst << endl;
+    }
+    void display()
+    {
+        for (int i = 1; i <= v; i++)
+        {
+            cout << "Node" << i << "->";
+            for (auto it : adj[i])
+            {
+                cout << "(" << it.first << "," << it.second << ")";
             }
             cout << endl;
         }
     }
-
-    int prims() {
-        vector<bool> visited(V + 1, false);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, 1}); // start from node 1
-        int sum = 0;
-
-        while (!pq.empty()) {
-            auto it = pq.top();
-            int wt = it.first;
-            int node = it.second;
-            pq.pop();
-
-            if (visited[node])
-                continue;
-
-            sum += wt;
-            visited[node] = true;
-
-            for (auto it : adj[node]) {
-                int neighbour = it.first;
-                int edgewt = it.second;
-                if (!visited[neighbour]) {
-                    pq.push({edgewt, neighbour});
-                }
-            }
-        }
-
-        return sum;
-    }
-
-    int kruskal() {
-        sort(edges.begin(), edges.end(), compare);
-        vector<int> parent(V + 1);
-        for (int i = 1; i <= V; i++) {
-            parent[i] = i;
-        }
-
-        int mstWeight = 0;
-        cout << "\nEdges in Kruskal's MST:\n";
-        for (auto edge : edges) {
-            int u = edge.u;
-            int v = edge.v;
-            int w = edge.weight;
-
-            int pu = findParent(u, parent);
-            int pv = findParent(v, parent);
-
-            if (pu != pv) {
-                cout << u << " - " << v << " : " << w << endl;
-                mstWeight += w;
-                parent[pu] = pv;
-            }
-        }
-
-        return mstWeight;
-    }
-
-    int findParent(int node, vector<int>& parent) {
-        while (node != parent[node])
-            node = parent[node];
-        return node;
-    }
 };
-
-int main() {
-    int V, E;
-    cout << "Enter number of vertices and edges: ";
-    cin >> V >> E;
-
-    Graph g(V);
-
-    cout << "Enter " << E << " edges in format: u v w\n";
-    for (int i = 0; i < E; i++) {
+int main()
+{
+    int v, e;
+    cout << "enter the number of vertices" << endl;
+    cin >> v;
+    cout << "enter the number of edges" << endl;
+    cin >> e;
+    graph g(v);
+    cout << "enter " << e << " edges in format :u,v,w" << endl;
+    for (int i = 0; i < e; i++)
+    {
         int u, v, w;
         cin >> u >> v >> w;
-        g.addEdge(u, v, w);
+        g.addedge(u, v, w);
     }
-
     int choice;
-    do {
-        cout << "\nMenu:\n";
-        cout << "1. Add Edge\n";
-        cout << "2. Display Graph\n";
-        cout << "3. Find MST using Prim's Algorithm\n";
-        cout << "4. Find MST using Kruskal's Algorithm\n";
-        cout << "5. Exit\n";
-        cout << "Enter your choice: ";
+    do
+    {
+        cout << "Menu" << endl;
+        cout << "1.add edge" << endl;
+        cout << "2.display graph" << endl;
+        cout << "3.prims" << endl;
+        cout << "4.kruskals" << endl;
+        cout << "5.exit" << endl;
         cin >> choice;
-
-        switch (choice) {
-        case 1: {
+        switch (choice)
+        {
+        case 1:
+        {
+            cout << "enter the edge in format u,v,w" << endl;
             int u, v, w;
-            cout << "Enter edge (u v w): ";
             cin >> u >> v >> w;
-            g.addEdge(u, v, w);
+            g.addedge(u, v, w);
             break;
         }
         case 2:
-            g.displayGraph();
+        {
+            g.display();
             break;
+        }
         case 3:
-            cout << "Minimum Spanning Tree Weight (Prim's): " << g.prims() << endl;
+        {
+            g.prims();
             break;
+        }
         case 4:
-            cout << "Minimum Spanning Tree Weight (Kruskal's): " << g.kruskal() << endl;
+        {
+            g.kruskals();
             break;
+        }
         case 5:
-            cout << "Exiting...\n";
-            break;
+        {
+            cout << "exiting...." << endl;
+            return 0;
+        }
         default:
-            cout << "Invalid choice. Try again.\n";
+        {
+            cout << "enter valid choice" << endl;
+            break;
+        }
         }
     } while (choice != 5);
-
     return 0;
 }
